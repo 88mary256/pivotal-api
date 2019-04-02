da2028c8687b8529d73cdd712cb9530dd456042d
rrchcr8 <CarlosRichter@fundacion-jala.org>
2019-04-02 12:12:02 -0400

    update features (less PUT)

--- features/steps/story_steps.py
+++ features/steps/story_steps.py
@@ -1,14 +1,11 @@
-import pprint
-
 from behave import *
-from compare import expect
 
 from src.core.utils.logger import logger_pivotal
 from src.pivotal_services.project_service import ProjectService
-
 from src.pivotal_services.story_service import StoryService
 
-project_id=""
+project_id = ""
+
 
 @given(u'Set up a connection')
 def step_impl(context):
@@ -19,7 +16,7 @@ def step_impl(context):
 @given(u'I create a project called {project_name}')
 def step_impl(context, project_name):
     global project_id
-    context.logger = logger_pivotal()
+
     context.project = ProjectService()
     context.project_name = project_name
     context.project_response = context.project.new_project(context.project_name)
@@ -28,35 +25,29 @@ def step_impl(context, project_name):
     context.logger.set_info(res_json)
     context.id_proj = res_json["id"]
     context.logger.set_info("id of the project: %s" % context.id_proj)
-    project_id=context.id_proj
+    project_id = context.id_proj
 
-@given (u'an story name: {story_name}')
+
+@given(u'an story name: {story_name}')
 def step_impl(context, story_name):
     context.story_name = story_name
 
-@then (u'the user makes a post to create the new story')
+
+@then(u'the user makes a post to create the new story')
 def step_impl(context):
-    context.logger = logger_pivotal()
-    context.story = StoryService()
     context.story_add = context.story.post_story(context.story_name, str(project_id))
     context.logger.set_info("POST Story status code: %s" % context.story_add.status_code)
     res_json2 = context.story_add.json()
     context.logger.set_info(res_json2)
 
 
-
 @given(u'I asks for the desired story: {story_name}')
 def step_impl(context, story_name):
     context.story_name = story_name
-    context.logger = logger_pivotal()
-    context.story = StoryService()
+
     context.story_response = context.story.get_all_stories(str(project_id))
     context.logger.set_info("GET ALL Story status code: %s" % context.story_response.status_code)
     context.res_json = context.story_response.json()
-
-
-    #context.logger.set_info(res_json[0])
-
 
 
 @then(u'The desired story is retrieved')
@@ -74,15 +65,9 @@ def step_impl(context):
 @given(u'I asks for the desired story to delete: {story_name}')
 def step_impl(context, story_name):
     context.story_name = story_name
-    context.logger = logger_pivotal()
-    context.story = StoryService()
     context.story_response = context.story.get_all_stories(str(project_id))
     context.logger.set_info("GET ALL Story status code: %s" % context.story_response.status_code)
     context.res_json = context.story_response.json()
-
-
-    #context.logger.set_info(res_json[0])
-
 
 
 @then(u'The desired story is deleted')
@@ -95,25 +80,25 @@ def step_impl(context):
             context.logger.set_info("DEL Story status code: %s" % context.story_response2.status_code)
 
 
-@given(u'a history name: {story_name}')
-def step_impl(context,story_name):
-    context.story_name = story_name
-    context.logger = logger_pivotal()
-    context.story = StoryService()
-    context.story_response = context.story.get_all_stories(str(project_id))
-    context.logger.set_info("GET ALL Story status code: %s" % context.story_response.status_code)
-    context.res_json = context.story_response.json()
+# @given(u'a history name: {story_name}')
+# def step_impl(context, story_name):
+#     context.story_name = story_name
+#     context.story_response = context.story.get_all_stories(str(project_id))
+#     #context.logger.set_info("GET ALL Story status code: %s" % context.story_response.status_code)
+#     context.res_json = context.story_response.json()
+#
+#     body = {"name": "cambiaso"}
+#     for i in range(len(context.res_json)):
+#         # pprint.pprint(res_json[i])
+#         if context.res_json[i]["name"] == str(context.story_name):
+#             story_id = context.res_json[i]["id"]
+#             context.logger.set_info(story_id)
+#             # for row in context.table:
+#             #   body[row['story_param']] = row['story_value']
+#
+#             context.story_response2 = context.story.put_story(str(story_id), str(project_id), body)
+#             context.logger.set_info("PUT Story status code: %s" % context.story_response2.status_code)
+# 
+# #@then(u'Modify according to the next parameters')
+# #def step_impl(context):
 
-
-@then(u'Modify according to the next parameters:')
-def step_impl(context, DataTable arg):
-    body={}
-    for i in range(len(context.res_json)):
-        # pprint.pprint(res_json[i])
-        if context.res_json[i]["name"] == str(context.story_name):
-            story_id = context.res_json[i]["id"]
-            for row in context.table:
-                body[row['story_param']] = row['story_value']
-
-            context.story_response2 = context.story.put_story(str(story_id), str(project_id),body)
-            context.logger.set_info("PUT Story status code: %s" % context.story_response2.status_code)
--- features/story.feature
+++ features/story.feature
@@ -2,10 +2,11 @@ Feature: Working with Stories on pivotal tracker
 
   Under this feature is covered all CRUD methods available under pivotaltracker.com API documentation.
 
-Given Set up a connection
+Background: set up
+  Given Set up a connection
 
-Scenario: : Create a project in order to CRUD Stories
-  Given I create a project called P39
+Scenario: : Create a project for all the following scenarios
+  Given I create a project called P46
 
 
   Scenario Outline: Create a new story
@@ -26,6 +27,7 @@ Scenario: : Create a project in order to CRUD Stories
       | story_name |
       | S1         |
       | S2         |
+      | S3         |
 
   Scenario Outline: Delete a specific Story by name
     Given I asks for the desired story to delete: <story_name>
@@ -37,10 +39,10 @@ Scenario: : Create a project in order to CRUD Stories
       | S2         |
 
 
-    Scenario: modify a Story
-      Given a history name: s3
-      Then: Modify according to the next parameters
-
-      |story_param   | story_value     |
-      |current_state | started         |
-      |name          | name_modified   |
+#    Scenario: modify a Story
+#      Given a history name: s3
+      #Then: Modify according to the next parameters
+#
+#      |story_param   | story_value     |
+#      |current_state | started         |
+#      |name          | name_modified   |
